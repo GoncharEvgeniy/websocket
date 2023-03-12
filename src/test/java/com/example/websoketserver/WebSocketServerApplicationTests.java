70fb9587d2d30a3428b65bff0c4dce12d82d1018
GoncharEvgeniy <uhp.gonchar.evgeniy@gmail.com>
2023-03-12 21:55:39 +0200

    Init commit

---
+++ src/test/java/com/example/websoketserver/WebSocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websoketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
